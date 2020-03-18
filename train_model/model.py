df703dcb5481a5ed2d580ffd9ac322268dfc87df
SilanHe <silanhe@outlook.com>
2020-03-18 15:22:06 -0400

    print of batch type in model

--- train_model/model.py
+++ train_model/model.py
@@ -28,6 +28,7 @@ class LSTMSentiment(nn.Module):
                             Variable(torch.zeros(1, batch.text.size()[1], self.hidden_dim)))
 
         # check if a batch or an input tensor
+        print(type(batch))
         if isinstance(type(batch),Batch):
             vecs = self.embed(batch.text)
         elif isinstance(type(batch),Tensor):
