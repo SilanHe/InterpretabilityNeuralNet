c8e78df79eed23ccef3c93fc42168bafe69ecb13
SilanHe <silanhe@outlook.com>
2020-03-20 13:38:03 -0400

    train.py space to tabs

--- train_model/train.py
+++ train_model/train.py
@@ -26,16 +26,16 @@ train, dev, test = datasets.SST.splits(inputs, answers, fine_grained = False, tr
 
 inputs.build_vocab(train, dev, test)
 if args.word_vectors:
-    if os.path.isfile(args.vector_cache):
-        inputs.vocab.vectors = torch.load(args.vector_cache)
-    else:
-        inputs.vocab.load_vectors(args.word_vectors)
-        makedirs(os.path.dirname(args.vector_cache))
-        torch.save(inputs.vocab.vectors, args.vector_cache)
+	if os.path.isfile(args.vector_cache):
+		inputs.vocab.vectors = torch.load(args.vector_cache)
+	else:
+		inputs.vocab.load_vectors(args.word_vectors)
+		makedirs(os.path.dirname(args.vector_cache))
+		torch.save(inputs.vocab.vectors, args.vector_cache)
 answers.build_vocab(train)
 
 train_iter, dev_iter, test_iter = data.BucketIterator.splits(
-            (train, dev, test), batch_size=args.batch_size, device=device)
+			(train, dev, test), batch_size=args.batch_size, device=device)
 
 config = args
 config.n_embed = len(inputs.vocab)
@@ -43,12 +43,12 @@ config.d_out = len(answers.vocab)
 config.n_cells = config.n_layers
 
 if args.resume_snapshot:
-    model = torch.load(args.resume_snapshot, map_location=lambda storage, location: storage.cuda(args.gpu))
+	model = torch.load(args.resume_snapshot, map_location=lambda storage, location: storage.cuda(args.gpu))
 else:
-    model = LSTMSentiment(config)
-    if args.word_vectors:
-        model.embed.weight.data = inputs.vocab.vectors
-        model.cuda()
+	model = LSTMSentiment(config)
+	if args.word_vectors:
+		model.embed.weight.data = inputs.vocab.vectors
+		model.cuda()
 
 criterion = nn.CrossEntropyLoss()
 opt = O.Adam(model.parameters())
@@ -65,71 +65,71 @@ print(header)
 
 all_break = False
 for epoch in range(args.epochs):
-    if all_break:
-        break
-    train_iter.init_epoch()
-    n_correct, n_total = 0, 0
-    for batch_idx, batch in enumerate(train_iter):
-        # switch model to training mode, clear gradient accumulators
-        model.train(); opt.zero_grad()
+	if all_break:
+		break
+	train_iter.init_epoch()
+	n_correct, n_total = 0, 0
+	for batch_idx, batch in enumerate(train_iter):
+		# switch model to training mode, clear gradient accumulators
+		model.train(); opt.zero_grad()
 
-        iterations += 1
+		iterations += 1
 
-        # forward pass
-        answer = model(batch)
+		# forward pass
+		answer = model(batch)
 
-        # calculate accuracy of predictions in the current batch
-        n_correct += (torch.max(answer, 1)[1].view(batch.label.size()).data == batch.label.data).sum()
-        n_total += batch.batch_size
-        train_acc = 100. * n_correct/n_total
+		# calculate accuracy of predictions in the current batch
+		n_correct += (torch.max(answer, 1)[1].view(batch.label.size()).data == batch.label.data).sum()
+		n_total += batch.batch_size
+		train_acc = 100. * n_correct/n_total
 
-        # calculate loss of the network output with respect to training labels
-        loss = criterion(answer, batch.label)
+		# calculate loss of the network output with respect to training labels
+		loss = criterion(answer, batch.label)
 
-        # backpropagate and update optimizer learning rate
-        loss.backward(); opt.step()
+		# backpropagate and update optimizer learning rate
+		loss.backward(); opt.step()
 
-        # checkpoint model periodically
-        if iterations % args.save_every == 0:
-            snapshot_prefix = os.path.join(args.save_path, 'snapshot')
-            snapshot_path = snapshot_prefix + '_acc_{:.4f}_loss_{:.6f}_iter_{}_model.pt'.format(train_acc, loss.item(), iterations)
-            torch.save(model, snapshot_path)
-            for f in glob.glob(snapshot_prefix + '*'):
-                if f != snapshot_path:
-                    os.remove(f)
+		# checkpoint model periodically
+		if iterations % args.save_every == 0:
+			snapshot_prefix = os.path.join(args.save_path, 'snapshot')
+			snapshot_path = snapshot_prefix + '_acc_{:.4f}_loss_{:.6f}_iter_{}_model.pt'.format(train_acc, loss.item(), iterations)
+			torch.save(model, snapshot_path)
+			for f in glob.glob(snapshot_prefix + '*'):
+				if f != snapshot_path:
+					os.remove(f)
 
-        # evaluate performance on validation set periodically
-        if iterations % args.dev_every == 0:
+		# evaluate performance on validation set periodically
+		if iterations % args.dev_every == 0:
 
-            # switch model to evaluation mode
-            model.eval(); dev_iter.init_epoch()
+			# switch model to evaluation mode
+			model.eval(); dev_iter.init_epoch()
 
-            # calculate accuracy on validation set
-            n_dev_correct, dev_loss = 0, 0
-            for dev_batch_idx, dev_batch in enumerate(dev_iter):
-                 answer = model(dev_batch)
-                 n_dev_correct += (torch.max(answer, 1)[1].view(dev_batch.label.size()).data == dev_batch.label.data).sum()
-                 dev_loss = criterion(answer, dev_batch.label)
-            dev_acc = 100. * n_dev_correct / len(dev)
+			# calculate accuracy on validation set
+			n_dev_correct, dev_loss = 0, 0
+			for dev_batch_idx, dev_batch in enumerate(dev_iter):
+				 answer = model(dev_batch)
+				 n_dev_correct += (torch.max(answer, 1)[1].view(dev_batch.label.size()).data == dev_batch.label.data).sum()
+				 dev_loss = criterion(answer, dev_batch.label)
+			dev_acc = 100. * n_dev_correct / len(dev)
 
-            print(dev_log_template.format(time.time()-start,
-                epoch, iterations, 1+batch_idx, len(train_iter),
-                100. * (1+batch_idx) / len(train_iter), loss.item(), dev_loss.item(), train_acc, dev_acc))
+			print(dev_log_template.format(time.time()-start,
+				epoch, iterations, 1+batch_idx, len(train_iter),
+				100. * (1+batch_idx) / len(train_iter), loss.item(), dev_loss.item(), train_acc, dev_acc))
 
-            # update best valiation set accuracy
-            if dev_acc > best_dev_acc:
-                best_dev_acc = dev_acc
-                snapshot_prefix = os.path.join(args.save_path, 'best_snapshot')
-                snapshot_path = snapshot_prefix + '_devacc_{}_devloss_{}_iter_{}_model.pt'.format(dev_acc, dev_loss.item(), iterations)
+			# update best valiation set accuracy
+			if dev_acc > best_dev_acc:
+				best_dev_acc = dev_acc
+				snapshot_prefix = os.path.join(args.save_path, 'best_snapshot')
+				snapshot_path = snapshot_prefix + '_devacc_{}_devloss_{}_iter_{}_model.pt'.format(dev_acc, dev_loss.item(), iterations)
 
-                # save model, delete previous 'best_snapshot' files
-                torch.save(model, snapshot_path)
-                for f in glob.glob(snapshot_prefix + '*'):
-                    if f != snapshot_path:
-                        os.remove(f)
+				# save model, delete previous 'best_snapshot' files
+				torch.save(model, snapshot_path)
+				for f in glob.glob(snapshot_prefix + '*'):
+					if f != snapshot_path:
+						os.remove(f)
 
-        elif iterations % args.log_every == 0:
-            # print progress message
-            print(log_template.format(time.time()-start,
-                epoch, iterations, 1+batch_idx, len(train_iter),
-                100. * (1+batch_idx) / len(train_iter), loss.item(), ' '*8, n_correct/n_total*100, ' '*12))
+		elif iterations % args.log_every == 0:
+			# print progress message
+			print(log_template.format(time.time()-start,
+				epoch, iterations, 1+batch_idx, len(train_iter),
+				100. * (1+batch_idx) / len(train_iter), loss.item(), ' '*8, n_correct/n_total*100, ' '*12))
